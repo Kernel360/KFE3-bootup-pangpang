6a0f4e6f7d2d6b3725ec6f59b5de1bf14536314c
Jiiker <motive46@naver.com>
2025-06-11 00:03:35 +0900

    Feat: 필요한 상수, 타입 설정

---
+++ packages/pds_ui/src/components/icons/icon-styles.ts
@@ -0,0 +1,6 @@
+export const COLOR_MAP = {
+  primary: 'text-color-brand-primary',
+  success: 'text-color-success-primary',
+  danger: 'text-color-danger-primary',
+  info: 'text-color-info-primary',
+} as const;
