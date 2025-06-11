bd6ba09c31a5b6d08807e2f9dcb97771a347f1cf
Jiiker <motive46@naver.com>
2025-06-11 09:52:35 +0900

    Chore: 상수 명명법 규칙 맞춰 수정

--- packages/pds_ui/src/components/icons/components/AngleDownIcon.tsx
+++ packages/pds_ui/src/components/icons/components/AngleDownIcon.tsx
@@ -1,7 +1,7 @@
 import { FaAngleDown } from 'react-icons/fa6';
 import { cn } from '../../../utils/cn';
 import type { IconProps } from '../icon-types';
-import { COLOR_MAP, SIZE_MAP } from '../icon-styles';
+import { COLORS, SIZES } from '../icon-styles';
 
 export const AngleDownIcon = ({
   size = 'md',
@@ -9,8 +9,8 @@ export const AngleDownIcon = ({
   className,
   ...props
 }: IconProps) => {
-  const sizeValue = SIZE_MAP[size];
-  const colorValue = color ? COLOR_MAP[color] : undefined;
+  const sizeValue = SIZES[size];
+  const colorValue = color ? COLORS[color] : undefined;
 
   const iconStyle = {
     width: sizeValue,
